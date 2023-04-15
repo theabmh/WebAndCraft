88b6781b374b14eda97c88d256e487f216c9dbef
Abbasmh <abbasabu3g@gmail.com>
2023-04-15 13:31:33 +0530

    Initial Commit

---
+++ WebAndCraft/WebAndCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebAndCraftApp.swift
+//  WebAndCraft
+//
+//  Created by abbas m h on 15/04/23.
+//
+
+import SwiftUI
+
+@main
+struct WebAndCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
